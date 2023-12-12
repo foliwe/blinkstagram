e6f546083be8b368e8106b5b9f4d826c0cecf05c
foliwe <foliwe@gmail.com>
2023-12-12 16:07:37 +0000

    categories added to post

--- post/forms.py
+++ post/forms.py
@@ -7,25 +7,30 @@ from .models import Post
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['url', 'body']
+        fields = ['url', 'body', 'tags']
         labels = {
             'body': 'Caption',
+            'tags': 'Categoty'
         }
 
         widgets = {
             'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption....', 'class': 'font1 text-4xl'}),
             'url': forms.URLInput(attrs={'placeholder': 'Add a url....', }),
+            'tags': forms.CheckboxSelectMultiple(attrs={'class': 'text-sm'})
         }
 
 
 class UpdatePostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['body']
+        fields = ['body', 'tags']
         labels = {
             'body': '',
+            'tags': 'Category'
+
         }
 
         widgets = {
-            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption....', 'class': 'font1 text-4xl'}),
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption....', 'class': 'font1 text-3xl'}),
+            'tags': forms.CheckboxSelectMultiple()
         }
